965ecc0b6d0f10fe2328c8dce0fdae7b117b5164
Pons <lanzosuarez@gmail.com>
2017-01-09 01:39:53 +0800

    fix

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var userRoutes = require('./routes/user');
 
 var app = express();
 
+//mongoose.connect('127.0.0.1:27017/node-angular');
 mongoose.connect('lanzosuarez:bobotngacla1234@ds159188.mlab.com:59188/angular2-deployment');
 
 // view engine setup
